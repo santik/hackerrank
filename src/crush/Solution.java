923be9650a7848e9ae13dac99233daea60d5d28a
Alexander Fedorets <alex.fedorets@quby.com>
2020-08-14 11:46:55 +0200

    add crush

---
+++ src/crush/Solution.java
@@ -0,0 +1,82 @@
+package crush;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the arrayManipulation function below.
+    static long arrayManipulation(int n, int[][] queries) {
+
+        long[] arr = new long[n + 2];
+
+        for (int[] query : queries) {
+            arr[query[0]] += query[2];
+            arr[query[1] + 1] -= query[2];
+        }
+
+        long sum = 0;
+        long max = 0;
+        for (long i: arr) {
+            sum += i;
+            max = Math.max(max, sum);
+        }
+
+        return max;
+    }
+
+
+    private static long naiveSolution(int n, int[][] queries) {
+        long[] array = new long[n];
+
+        long max = 0;
+        for (int[] query : queries) {
+
+            for (int j = query[0] - 1; j < query[1]; j++) {
+                array[j] = array[j] + query[2];
+                max = Math.max(max, array[j]);
+            }
+        }
+
+        return max;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+//        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] nm = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nm[0]);
+
+        int m = Integer.parseInt(nm[1]);
+
+        int[][] queries = new int[m][3];
+
+        for (int i = 0; i < m; i++) {
+            String[] queriesRowItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int j = 0; j < 3; j++) {
+                int queriesItem = Integer.parseInt(queriesRowItems[j]);
+                queries[i][j] = queriesItem;
+            }
+        }
+
+        long result = arrayManipulation(n, queries);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
